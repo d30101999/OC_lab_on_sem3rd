b0912230034374313ed7e54831827162f487d9e3
d30101999 <58314095+d30101999@users.noreply.github.com>
2020-11-27 19:53:01 +0300

    Update def_gen.sh

--- LAB3/def_gen.sh
+++ LAB3/def_gen.sh
@@ -1,3 +1,4 @@
+//The first script in an endless cycle gets a list of processes and writes the first PID (or any PID) in a log file. A log format: “PID is working and soon going on vacation.” A second script once in 3 seconds checks the log file. If there is a PID > 1000, sends a signal to the first script, kills it, and writes in the log file “Wasted.”
 #!/bin/bash
 echo $$ > .pid
 echo "" > deflab3.log
